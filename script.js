95d82e14155c6114f6c173e6e8ae0b29d07b61ca
EvgenMironov900 <evgem@bk.ru>
2025-08-09 23:09:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-document.getElementsByTagName("h2")[0].style.fontSize = "6vw";
-// document.getElementsByTagName("h2")[0].style.fontSize = "10vw";
+// document.getElementsByTagName("h2")[0].style.fontSize = "6vw";
+document.getElementsByTagName("h2")[0].style.fontSize = "10vw";
