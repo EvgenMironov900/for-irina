924f2182ced754b9940557138fb5f30f30fe5a31
EvgenMironov900 <evgem@bk.ru>
2025-08-09 22:56:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+const h2Elements = document.getElementsByTagName("h2");
+if (h2Elements.length > 0) {
+  h2Elements[0].style.fontSize = "6vw";
+} else {
+  console.error("Элемент <h2> не найден!");
+}
 document.getElementsByTagName("h1")[0].style.fontSize = "6vw";
